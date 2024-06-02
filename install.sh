630aab8b0c54e6c22086b0decf67ec5e086cb302
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-02 01:48:24 -0300

    Commit

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ set -euo pipefail
 mkdir -p "$HOME/.config/alacritty"
 rm -rf "$HOME/.config/alacritty"
 ln -sf "$HOME/dotfiles/alacritty" "$HOME/.config/alacritty"
+
+# fonts
+mkdir -p "$HOME/.local/share/fonts"
+rm -rf "$HOME/.local/share/fonts"
+ln -sf "$HOME/dotfiles/fonts" "$HOME/.local/share/fonts"
