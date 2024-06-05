8df0d3448de0a400c9a4967c31bf298c3dde55c7
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-05 19:30:39 -0300

    Commit

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -7,6 +7,41 @@ return {
 		"MunifTanjim/nui.nvim"
 	},
 	config = function()
-		vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', { noremap = true, silent = true })
+		require('neo-tree').setup({
+			close_if_last_window = true,
+			window = {
+				width = 40,
+			},
+			filesystem = {
+				use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes instead of relying on nvim autocmd events.
+				group_empty_dirs = false, -- when true, empty folders will be grouped together
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
+					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+				},
+				filtered_items = {
+					hide_dotfiles = true,
+					hide_gitignored = true,
+					hide_by_name = {
+						--"node_modules"
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*.meta",
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						--".gitignored",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						--".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
+				},
+			}
+		})
+		vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { noremap = true, silent = true })
 	end
 }
--- neovim/lua/vim-options.lua
+++ neovim/lua/vim-options.lua
@@ -39,3 +39,16 @@ vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+
+
+-- Render whitespace characters
+vim.opt.list = true
+vim.opt.listchars = {
+	space = '·',   -- Show spaces as dots
+	tab = '→ ',    -- Show tabs as arrows followed by a space
+	trail = '•',   -- Show trailing spaces as bullets
+	extends = '▶', -- Show lines that extend beyond the screen as arrows
+	precedes = '◀',-- Show lines that extend before the screen as arrows
+	nbsp = '␣'     -- Show non-breaking spaces as a different symbol
+}
+
