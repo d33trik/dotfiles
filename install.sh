530a8fe200e1b22827e8a338f623f8329e10993d
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-05 00:18:23 -0300

    Commit

--- install.sh
+++ install.sh
@@ -2,11 +2,21 @@
 set -euo pipefail
 
 # alacritty
-mkdir -p "$HOME/.config/alacritty"
 rm -rf "$HOME/.config/alacritty"
 ln -sf "$HOME/dotfiles/alacritty" "$HOME/.config/alacritty"
 
-# fonts
-mkdir -p "$HOME/.local/share/fonts"
-rm -rf "$HOME/.local/share/fonts"
-ln -sf "$HOME/dotfiles/fonts" "$HOME/.local/share/fonts"
+# gtk
+ln -sf "$HOME/dotfiles/gtk/gtk-2/gtkrc-2.0" "$HOME/.gtkrc-2.0"
+ln -sf "$HOME/dotfiles/gtk/gtk-3/settings.ini" "$HOME/.config/gtk-3.0/settings.ini"
+
+# i3 
+rm -rf "$HOME/.config/i3"
+ln -sf "$HOME/dotfiles/i3" "$HOME/.config/i3"
+
+# neovim
+rm -rf "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/neovim" "$HOME/.config/nvim"
+
+# x
+ln -sf "$HOME/dotfiles/x/xinitrc" "$HOME/.xinitrc"
+ln -sf "$HOME/dotfiles/x/Xresources" "$HOME/.Xresources"
---
+++ neovim/lua/vim-options.lua
@@ -0,0 +1,38 @@
+-- Set leader key to space
+vim.g.mapleader = " "
+
+-- Use the system clipboard for all yank, delete, change, and put operations
+vim.opt.clipboard:append("unnamedplus")
+
+-- Disable the swap file creation
+vim.opt.swapfile = false
+
+-- Show line numbers
+vim.opt.number = true
+
+-- Enable automatic indentation
+vim.opt.autoindent = true
+
+-- Do use tabs instead of spaces
+vim.opt.expandtab = false
+
+-- Number of spaces that a <Tab> counts for
+vim.opt.tabstop = 4
+
+-- Number of spaces that a <Tab> counts for while editing
+vim.opt.softtabstop = 4
+
+-- Number of spaces to use for each step of (auto)indent
+vim.opt.shiftwidth = 4
+
+-- Mapping <leader>w to save the file
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+
+-- Mapping: <leader>q to quit
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+
+-- Disable the arrow keys
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
