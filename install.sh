b9eea67aa8706b1161555786fb22c11df00c1aa5
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-05 00:22:39 -0300

    Commit

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ ln -sf "$HOME/dotfiles/gtk/gtk-3/settings.ini" "$HOME/.config/gtk-3.0/settings.i
 rm -rf "$HOME/.config/i3"
 ln -sf "$HOME/dotfiles/i3" "$HOME/.config/i3"
 
+#icons
+ln -sf "$HOME/dotfiles/icons/index/index.theme" "$HOME/.local/share/icons/default/index.theme"
+ln -sf "$HOME/dotfiles/icons/index/index.theme" "$HOME/.icons/default/index.theme"
+
 # neovim
 rm -rf "$HOME/.config/nvim"
 ln -sf "$HOME/dotfiles/neovim" "$HOME/.config/nvim"
