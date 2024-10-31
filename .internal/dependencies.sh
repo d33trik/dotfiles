04874a41401f364762134455eff796ffdcbd2c32
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-31 19:09:37 -0300

    Install missing dependencies

--- .internal/dependencies.sh
+++ .internal/dependencies.sh
@@ -9,7 +9,24 @@ check_dependencies() {
 	done
 
 	if (( ${#missing_deps[@]} > 0 )); then
-		echo "Missing required dependencies: ${missing_deps[*]}"
-		exit 1
+		echo "Installing missing dependencies: ${missing_deps[*]}"
+
+		for missing_dep in "${missing_deps[@]}"; do
+			echo "Installing ${missing_dep}..."
+			install_"$missing_dep"
+		done
 	fi
+}
+
+install_gum() {
+	sudo pacman -S --noconfirm --needed gum
+}
+
+install_yay() {
+	sudo pacman -S --noconfirm --needed git base-devel
+	cd /tmp
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg --noconfirm -si
+	sudo pacman -Rs --noconfirm go
 }
