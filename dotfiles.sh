a3e29e4815956b2439d8a6316fa63cc4cd14e752
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-31 19:16:38 -0300

    Display menu

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,7 +10,24 @@ export DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
 source "${DOTFILES_DIR}/.internal/dependencies.sh"
 
 main() {
+	local action
+
 	check_dependencies
+	display_menu
+}
+
+display_menu() {
+	local options=(
+		"Apply settings"
+		"Install packages"
+		"Exit"
+	)
+
+	action=$(
+		printf "%s\n" "${options[@]}" |
+		gum choose \
+			--header="Dotfiles!"
+	)
 }
 
 main "$@"
