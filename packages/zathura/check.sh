b4cd02a12f8f468d072c9b57115e1c6a0cfe4b30
Claudio Dietrich <claudio@dietrich.dev.br>
2024-11-02 00:54:10 -0300

    Add package: zathura

---
+++ packages/zathura/check.sh
@@ -0,0 +1,3 @@
+set -euo pipefail
+
+pacman -Q zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
---
+++ packages/zathura/install.sh
@@ -0,0 +1,3 @@
+set -euo pipefail
+
+sudo pacman -S --noconfirm --needed zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
