f62716f291a90dfa773730596544abad5125c4de
Claudio Dietrich <d33trik@gmail.com>
2025-04-29 22:54:10 -0300

    Scripts: add backup script

---
+++ scripts/backup_create.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# e - script stops on error
+# u - error if undefined variable
+# o pipefail - script fails if command piped fails
+set -euo pipefail
+
+dest="/run/media/$(whoami)/backup/backup.tar.gz"
+backup_files=(
+	".config/BraveSoftware/Brave-Browser"
+	".config/chromium"
+	".local/share/fonts/dank-mono"
+	".ssh"
+)
+
+echo "Files to backup:"
+for file in "${backup_files[@]}"; do
+	echo " - $file"
+done
+
+gum spin \
+	--title="Creating backup at ${dest}..." \
+	--show-error="true" \
+	-- tar -czf "${dest}" -C "${HOME}" "${backup_files[@]}"
+
+gum spin \
+	--title="Syncing cached writes to persistent storage" \
+	--show-error="true" \
+	-- sync
+
+echo "✓ Backup successfully created at ${dest}"
