c7cd8db35e72bbf09885d2cdb104c124fe6c14ff
Claudio Dietrich <d33trik@gmail.com>
2025-09-19 20:52:33 -0300

    Add post-installation script
    Create a post_install.sh script to automate the final setup steps
    after cloning the dotfiles.
    
    This script performs the following actions:
    - Sets the git remote URL to the SSH address for future pushes.
    - Executes the install_packages.sh script to install all configured
    system packages and applications.
    
    This automation simplifies the setup process on a new machine,
    ensuring that the environment is configured consistently and correctly
    with a single command.

---
+++ scripts/post_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# e - script stops on error
+# u - error if undefined variable
+# o pipefail - script fails if command piped fails
+set -euo pipefail
+
+main() {
+	local DOTFILES_DIR="$HOME/dotfiles"
+
+	post_install
+}
+
+post_install() {
+	cd "$DOTFILES_DIR"
+	git remote set-url origin git@github.com:d33trik/dotfiles.git
+	bash scripts/install_packages.sh
+}
+
+main "$@"
