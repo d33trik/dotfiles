2edb61c1454c9e0aa85b55f7579b815c775344ae
Claudio Dietrich <d33trik@gmail.com>
2026-04-19 20:22:04 -0300

    Add dunst theme support to change_theme.sh

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -17,6 +17,7 @@ main() {
 	theme=$(gum choose "${THEMES[@]}")
 
 	change_alacritty_theme "$theme"
+	change_dunst_theme "$theme"
 }
 
 change_alacritty_theme() {
@@ -24,6 +25,25 @@ change_alacritty_theme() {
 	local theme="$1"
 
 	sed -i "s|~/.config/alacritty/themes/[^\"]*\.toml|~/.config/alacritty/themes/${theme}.toml|g" "$ALACRITTY_CONFIG"
+}
+
+change_dunst_theme() {
+	readonly DUNST_CONFIG="$HOME/dotfiles/.config/dunst/dunstrc"
+	readonly DUNST_THEMES_DIR="$HOME/dotfiles/.config/dunst/themes"
+	local theme="$1"
+	local theme_file="$DUNST_THEMES_DIR/${theme}.dunstrc"
+
+	if [[ ! -f "$theme_file" ]]; then
+		echo "Error: theme file '$theme_file' not found" >&2
+		return 1
+	fi
+
+	sed -i '/^\[urgency_/,/^$/d' "$DUNST_CONFIG"
+
+	cat "$theme_file" >>"$DUNST_CONFIG"
+
+	pkill dunst || true
+	dunst &
 }
 
 main "$@"
