621b02063bbb5c5187718ab130f6af18565cf514
Claudio Dietrich <d33trik@gmail.com>
2026-04-19 22:30:09 -0300

    Add i3 theme support to change_theme.sh

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -19,6 +19,7 @@ main() {
 	change_alacritty_theme "$theme"
 	change_dunst_theme "$theme"
 	change_fish_theme "$theme"
+	change_i3_theme "$theme"
 }
 
 change_alacritty_theme() {
@@ -52,6 +53,36 @@ change_fish_theme() {
 	local theme="$1"
 
 	sed -i "s|fish_config theme choose .*|fish_config theme choose ${theme}|" "$FISH_CONFIG"
+}
+
+change_i3_theme() {
+	readonly I3_CONFIG="$HOME/dotfiles/.config/i3/config"
+	readonly I3STATUS_CONFIG="$HOME/dotfiles/.config/i3/i3status.conf"
+	readonly I3_THEMES_DIR="$HOME/dotfiles/.config/i3/themes"
+	local theme="$1"
+	local i3_theme_file="$I3_THEMES_DIR/${theme}.i3theme"
+	local i3status_theme_file="$I3_THEMES_DIR/${theme}.i3status"
+
+	if [[ ! -f "$i3_theme_file" ]]; then
+		echo "Error: theme file '$i3_theme_file' not found" >&2
+		return 1
+	fi
+
+	if [[ ! -f "$i3status_theme_file" ]]; then
+		echo "Error: theme file '$i3status_theme_file' not found" >&2
+		return 1
+	fi
+
+	sed -i "/^# Colors$/,/^set \$red/{
+        /^# Colors$/r $i3_theme_file
+        d
+    }" "$I3_CONFIG"
+
+	sed -i "s/color_good=\"[^\"]*\"/$(grep 'color_good' "$i3status_theme_file")/" "$I3STATUS_CONFIG"
+	sed -i "s/color_degraded=\"[^\"]*\"/$(grep 'color_degraded' "$i3status_theme_file")/" "$I3STATUS_CONFIG"
+	sed -i "s/color_bad=\"[^\"]*\"/$(grep 'color_bad' "$i3status_theme_file")/" "$I3STATUS_CONFIG"
+
+	i3-msg restart >/dev/null
 }
 
 main "$@"
