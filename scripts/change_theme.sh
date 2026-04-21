3be43966c870f3a3ca648602595b805648d97b3e
Claudio Dietrich <d33trik@gmail.com>
2026-04-20 21:35:50 -0300

    Add starship theme support to change_theme.sh

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -22,6 +22,7 @@ main() {
 	change_i3_theme "$theme"
 	change_neovim_theme "$theme"
 	change_lualine_theme "$theme"
+	change_starship_theme "$theme"
 }
 
 change_alacritty_theme() {
@@ -108,6 +109,64 @@ change_lualine_theme() {
 	while IFS='=' read -r key value; do
 		sed -i "s|nvim_set_hl(0, \"${key}\", { fg = \"[^\"]*\" })|nvim_set_hl(0, \"${key}\", { fg = ${value} })|" "$NVIM_LUALINE"
 	done <"$theme_file"
+}
+
+change_starship_theme() {
+	readonly STARSHIP_CONFIG="$HOME/dotfiles/.config/starship/starship.toml"
+	readonly STARSHIP_THEMES_DIR="$HOME/dotfiles/.config/starship/themes"
+	local theme="$1"
+	local theme_file="$STARSHIP_THEMES_DIR/${theme}.toml"
+
+	if [[ ! -f "$theme_file" ]]; then
+		echo "Error: theme file '$theme_file' not found" >&2
+		return 1
+	fi
+
+	local directory_style
+	directory_style=$(grep '^directory_style' "$theme_file" | cut -d'"' -f2)
+
+	local directory_read_only_style
+	directory_read_only_style=$(grep '^directory_read_only_style' "$theme_file" | cut -d'"' -f2)
+
+	local git_branch_style
+	git_branch_style=$(grep '^git_branch_style' "$theme_file" | cut -d'"' -f2)
+
+	local git_state_style
+	git_state_style=$(grep '^git_state_style' "$theme_file" | cut -d'"' -f2)
+
+	local git_status_style
+	git_status_style=$(grep '^git_status_style' "$theme_file" | cut -d'"' -f2)
+
+	local character_success
+	character_success=$(grep '^character_success' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	local character_error
+	character_error=$(grep '^character_error' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	local character_vimcmd
+	character_vimcmd=$(grep '^character_vimcmd' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	local character_vimcmd_visual
+	character_vimcmd_visual=$(grep '^character_vimcmd_visual' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	local character_vimcmd_replace
+	character_vimcmd_replace=$(grep '^character_vimcmd_replace' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	local character_vimcmd_replace_one
+	character_vimcmd_replace_one=$(grep '^character_vimcmd_replace_one' "$theme_file" | head -1 | cut -d'"' -f2)
+
+	sed -i "/^\[directory\]/,/^\[/ s|^style = '.*'|style = '${directory_style}'|" "$STARSHIP_CONFIG"
+	sed -i "/^\[directory\]/,/^\[/ s|^read_only_style = '.*'|read_only_style = '${directory_read_only_style}'|" "$STARSHIP_CONFIG"
+	sed -i "/^\[git_branch\]/,/^\[/ s|^style = '.*'|style = '${git_branch_style}'|" "$STARSHIP_CONFIG"
+	sed -i "/^\[git_state\]/,/^\[/ s|^style = '.*'|style = '${git_state_style}'|" "$STARSHIP_CONFIG"
+	sed -i "/^\[git_status\]/,/^\[/ s|^style = '.*'|style = '${git_status_style}'|" "$STARSHIP_CONFIG"
+
+	sed -i '/success_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_success"')/g' "$STARSHIP_CONFIG"
+	sed -i '/error_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_error"')/g' "$STARSHIP_CONFIG"
+	sed -i '/vimcmd_visual_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_vimcmd_visual"')/g' "$STARSHIP_CONFIG"
+	sed -i '/vimcmd_replace_one_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_vimcmd_replace_one"')/g' "$STARSHIP_CONFIG"
+	sed -i '/vimcmd_replace_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_vimcmd_replace"')/g' "$STARSHIP_CONFIG"
+	sed -i '/vimcmd_symbol/ s/(#[a-fA-F0-9]\{6\})/('"$character_vimcmd"')/g' "$STARSHIP_CONFIG"
 }
 
 main "$@"
