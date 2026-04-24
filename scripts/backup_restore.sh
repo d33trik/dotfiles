dffc4992b6b55c24443e4f7fe5713be23668edea
Claudio Dietrich <d33trik@gmail.com>
2026-04-24 17:32:07 -0300

    Add mount/umount steps to backup restore script

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -8,6 +8,11 @@ set -euo pipefail
 source="/run/media/$(whoami)/backup/backup.tar.gz"
 
 gum spin \
+	--title="Mounting device..." \
+	--show-error="true" \
+	-- udiskie-mount --all
+
+gum spin \
 	--title="Restoring backup from ${source}..." \
 	--show-error="true" \
 	-- tar -xzf "${source}" -C "${HOME}"
@@ -16,5 +21,10 @@ gum spin \
 	--title="Syncing cached writes to persistent storage" \
 	--show-error="true" \
 	-- sync
+
+gum spin \
+	--title="Unmounting device..." \
+	--show-error="true" \
+	-- udiskie-umount --all
 
 echo "✓ Backup successfully restored from ${source}"
