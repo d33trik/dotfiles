a2e03db869c1251d077d139bfa815b47ebd1a252
Claudio Dietrich <d33trik@gmail.com>
2026-08-01 23:46:14 -0300

    Rebind screensaver, add shutdown shortcut

--- scripts/gnome_settings.sh
+++ scripts/gnome_settings.sh
@@ -72,7 +72,8 @@ set_shortcuts() {
 	done
 	gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
 	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
-	gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "['<Ctrl>l']"
+	gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "['<Super>space']"
+	gsettings set org.gnome.settings-daemon.plugins.media-keys shutdown "['<Super>dead_grave']"
 	gsettings set org.gnome.desktop.wm.keybindings close "['<Super><Shift>q']"
 	gsettings set org.gnome.desktop.wm.keybindings minimize "[]"
 	echo "Shortcuts configured."
